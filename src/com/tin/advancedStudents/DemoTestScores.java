c9a29ffe3fa379d131aa35e6f057d5bff956ec2d
Sn-Win <tintrungn@gmail.com>
2015-10-22 11:41:36 -0700

    Revert "Revert "yes""
    This reverts commit f7841667fed1d48fa22deddb6ff22b5be435e7e7.

---
+++ src/com/tin/advancedStudents/DemoTestScores.java
@@ -0,0 +1,9 @@
+package com.tin.advancedStudents;
+
+public class DemoTestScores
+{
+  public static void main(String[] args) {
+   TestScoresModel aModel = new TestScoresModel();
+   TestScoresView aView = new TestScoresView(aModel);
+  }
+}
